7e18a44b388018d072ed1461f26a8ff2b98ecceb
Unknown <andrewpetersell@gmail.com>
2016-11-28 14:33:54 -0500

    Corgis and New Movement

---
+++ Dan Project/Assets/Scripts/CorgiAnimations.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CorgiAnimations : MonoBehaviour {
+
+public bool moving;
+public bool barking;
+public bool spooped;
+
+
+	// Use this for initialization
+	void Start () {
+
+	moving = false; 
+	barking = false;
+	spooped = false; 
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	stateDetector(); 
+	animationHandler();
+	
+	}
+
+	void stateDetector () 
+	{
+		if (Input.GetButton("Move"))
+		{
+			moving = true;
+		}
+		else 
+		{
+			moving = false;
+		}
+	}
+
+	void animationHandler ()
+	{
+		GetComponent<Animator>().SetBool("Moving", moving);
+		GetComponent<Animator>().SetBool("Barking", barking);
+		GetComponent<Animator>().SetBool("Spooped", spooped);
+	}
+}
---
+++ Dan Project/Assets/Scripts/NewPlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewPlayerMovement :MonoBehaviour {
+
+Camera mainCam;
+
+public float lerpSpeed;
+public float lerpPercent;
+public Vector3 currentPos;
+public Vector3 newPos;
+
+
+	// Use this for initialization
+	void Start () {
+
+	mainCam = Camera.main;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		currentPos = transform.position;
+		lerpPercent += Time.deltaTime/lerpSpeed; 
+		Vector3 mousePos = mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mainCam.transform.position.y)); 
+		transform.LookAt (mousePos + Vector3.up * transform.position.y);
+		newPos = mousePos + Vector3.up * transform.position.y;
+		movement();
+	
+	}
+
+	void movement () 
+	{
+		if (Input.GetButton("Move"))
+		{
+			Debug.Log("Moving");
+			transform.position = Vector3.Lerp(currentPos, newPos, lerpPercent);
+		}
+	}
+}
