4f4a08b06d87f7991ca613c4adfd3e73d1846888
Jenna <jenna.galka@gmail.com>
2016-12-02 22:41:55 -0500

    Capsule collider for light beam
    Because I gave up. Will fix Ghost Evac script to figure out how to actually, um, work it. Other changes trivial.

--- Dan Project/Assets/Scripts/GhostEvac.cs
+++ Dan Project/Assets/Scripts/GhostEvac.cs
@@ -1,43 +1,92 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GhostEvac : MonoBehaviour {
 
+	//objects and scripts
 	GameObject corgi;
-	SpoopScript spoop;
-	DogBeenSpooped ds;
+	GameObject capsule;
+	Collider beam;
+
+	//public stuff
 	public bool inLineSight = false;
+	public TextMesh spoopStatements;
+	public Vector3 pointA;
+	public Vector3 pointB;
+	public float newPointA;
+	public float newPointB;
+
+	public List<string> SpoopedWords;
+
+	//private stuff
 	private NavMeshAgent agent;
+	private int currentSpoopWord;
 
 	// Use this for initialization
 	void Start () {
 
 		//gettin' the scrips
 		corgi = GameObject.Find ("Player");
-		spoop = corgi.GetComponent<SpoopScript> ();
-		ds = corgi.GetComponent<DogBeenSpooped> ();
+		capsule = GameObject.Find ("Capsule");
+
+		beam = capsule.GetComponent<CapsuleCollider> ();
+
+		//fv = corgi.GetComponent<FieldOfView> ();
 
 		//navmesh
 		agent = this.GetComponent<NavMeshAgent>();
+
+		//settin' up the things they say
+		int i = 0;
+		foreach (string s in SpoopedWords) {
+			SpoopedWords [i] = s.Replace ("BREAK", "\r\n");
+			i++;
+		}
+		currentSpoopWord = 0;
+		spoopStatements.text = SpoopedWords [currentSpoopWord];
 	
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		Vector3 ghostDirection = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
-		float originalSpeed = agent.speed;
 
-		if (ds.visibleGhosts.Contains (this.transform)) {
 
-			inLineSight = true;
-			this.transform.position -= this.transform.forward * this.agent.speed;
+//		Vector3 pointA = fv.pointA;
+//		Vector3 pointB = fv.pointB;
+//
+//		float newPointA = pointA.magnitude;
+//		float newPointB = pointB.magnitude;
+//
+//		float Arc = newPointA - newPointB;
+//
+//		Vector3 newDistance = Vector3.Distance (newPointA, newPointB, this.transform);
+//
+//		if (ds.visibleGhosts.Contains (this.transform) && this.transform > newDistance) {
+//			inLineSight = true;
+//		}
+//
+		//GameObject[] ghostsHere = GameObject.FindGameObjectsWithTag ("Ghost");
 
-		} else {
-			
-			inLineSight = false;
-			this.agent.speed = originalSpeed;
-		}
+//		Vector3 arc = new Vector3 (newPointA.x, newPointB.y, newPointB.z);
+//		arc.Normalize;
+
+//		if (this.transform <= newPointA && this.transform >= newPointB) {
+//			inLineSight = true;
+//		}
+
+
+//		if (ds.visibleGhosts.Contains (this.transform)) {
+//
+//			inLineSight = true;
+//			this.transform.position -= this.transform.forward * this.agent.speed;
+//
+//		} else {
+//			
+//			inLineSight = false;
+//			this.agent.speed = originalSpeed;
+//		}
 		
 //		Vector3 targetDirection = corgi.transform.position - this.transform.position; //gets direction of target 
 //		float targetAngle = Vector3.Angle(targetDirection,this.transform.forward); //gets angle to target
@@ -46,5 +95,17 @@ public class GhostEvac : MonoBehaviour {
 //			Debug.Log ("Ghost Spoop");
 //			}
 	
+	}
+
+	void OnTriggerEnter(Collider col){
+
+//		Vector3 ghostDirection = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+//		float originalSpeed = agent.speed;
+
+		if (col = beam) {
+			inLineSight = true;
+		} else {
+			inLineSight = false;
+		}
 	}
 }
